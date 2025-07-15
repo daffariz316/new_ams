ab7a1070e4b37a095095346c0b1c9af11f6cdadd
daffariz316 <73021923+daffariz316@users.noreply.github.com>
2025-07-15 15:35:04 +0700

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import urllib.parse
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static',static_folder='../static/assets/imgs/clients' )
+app = Flask(__name__, template_folder='../templates', static_folder='../static' )
 
 # ===============================
 # ROUTE UTAMA
