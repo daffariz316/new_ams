4f959f4a88cb35edd24c2396c3be7f3842bff737
daffariz316 <73021923+daffariz316@users.noreply.github.com>
2025-07-15 15:02:48 +0700

    push

---
+++ api/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect
+import urllib.parse
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+
+# ===============================
+# ROUTE UTAMA
+# ===============================
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# ===============================
+# ROUTE HANDLE FORM CONTACT
+# ===============================
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form['subject']
+    message = request.form['message']
+
+    # Nomor WhatsApp tujuan (tanpa spasi)
+    whatsapp_number = '62895372499072'  # Ganti dengan nomor kamu
+
+    # Buat pesan WhatsApp
+    text = f"Halo! Saya {name} {email} {subject} {message}"
+
+    # Encode ke URL
+    encoded_text = urllib.parse.quote(text)
+
+    # Buat URL WhatsApp API
+    whatsapp_url = f"https://wa.me/{whatsapp_number}?text={encoded_text}"
+
+    # Redirect ke WhatsApp
+    return redirect(whatsapp_url)
+
+# # ===============================
+# # RUN SERVER
+# # ===============================
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
